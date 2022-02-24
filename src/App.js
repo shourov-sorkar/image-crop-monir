8ac6ec183e1b72b5eccc6a3ed1589f8a633a25a9
shourov-sorkar <18103142@iubat.edu>
2022-02-24 11:40:57 +0600

    :tada: Cropping done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Test from "./Components/Test"
+import Test2 from './Components/Test2';
+function App() {
+  return (
+    <div className="App">
+      <Test/>
+      {/* <Test2/> */}
+    </div>
+  );
+}
+
+export default App;
